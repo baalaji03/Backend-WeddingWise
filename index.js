6bb51f8b2d8e29326bb8f1ca2acac4cc9b5363a1
baalaji03 <rps123a@gmail.com>
2024-07-04 21:43:57 -0700

    updatedd

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
-    Credentials: true,
+    origin: "https://wedding-event-frontend.netlify.app",
+    credentials: true,
   })
 );
 
 app.use(express.json());
 
-//error handler
-
+// Error handler
 app.use((err, req, res, next) => {
+  console.error("Error:", err.message);
   const statusCode = err.statusCode || 500;
-  const message = err.message || `Internal Server Error`;
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
@@ -32,11 +32,13 @@ app.use((err, req, res, next) => {
 connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to api");
+  res.send("Welcome to API");
 });
-//API Routes
+
+// API Routes
 app.use("/api/auth", authRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on Port `);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
 });
